f9fdec990b7c9b4c79a48c3b2ffe95e2835f2823
edd <hiuhuedd@gmail.com>
2022-12-23 00:00:21 +0300

    first commit

--- index.js
+++ index.js
@@ -31,13 +31,20 @@ const isAuth = require("./middleware/is-auth");
 const house = require("./models/house");
 
 const Actives = require("./models/activeusers");
+const cors=require("cors"); 
 
-const app = express();
+const app = express(); 
+const corsOptions ={
+   origin:'*',  
+   credentials:true,             //access-control-allow-credentials:true
+   optionSuccessStatus:200, 
+} 
 
+app.use(cors(corsOptions)) 
 app.use(express.json());
 app.use(isAuth);
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:3000"); 
+  res.setHeader("Access-Control-Allow-Origin","*"); 
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
