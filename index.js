7e45ff77cfad4fe332f28f62a5999c555cf0be5a
edd <hiuhuedd@gmail.com>
2022-12-22 23:34:18 +0300

    first commit

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const app = express();
 app.use(express.json());
 app.use(isAuth);
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:3000"); 
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
