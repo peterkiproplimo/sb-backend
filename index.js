7f5b6f8c28fecc2c4e694b3d702720ad189220b0
peterkiprop <kiproplimo8@gmail.con>
2023-09-13 21:48:04 +0300

    latest changes

--- index.js
+++ index.js
@@ -58,7 +58,10 @@ app.use(express.json());
 app.use(isAuth);
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, UPDATE"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
