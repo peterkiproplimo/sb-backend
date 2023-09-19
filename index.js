32df71c671f0c68156e7812dda655e8eb1bd5936
peterkiprop <kiproplimo8@gmail.con>
2023-09-19 22:20:51 +0300

    latest changes

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, UPDATE"
   );
-  res.setHeader("Access-Control-Allow-Credentials", "false");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);
