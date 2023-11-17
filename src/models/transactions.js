452869b5490203d409e9f9d02d60e5e395eb23b9
peterkiproplimo <kiproplimo8@gmail.com>
2023-11-17 12:02:29 +0300

    Add types for paybill

--- src/models/transactions.js
+++ src/models/transactions.js
@@ -37,6 +37,13 @@ const transactionSchema = new Schema(
       required: false,
     },
 
+    //  For Paybill
+
+    TransID: {
+      type: String,
+      required: false,
+    },
+
     mpesaReceiptNumber: {
       type: String,
       required: false,
